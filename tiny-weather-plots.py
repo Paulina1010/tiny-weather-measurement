295abc2ed3014f01e7fc2ff9a8b48565c400b418
Paulina1010 <paulina.kucinska10@gmail.com>
2024-09-16 22:23:44 +0200

    Prepare data to the plot, execute as many queries as data sources

--- tiny-weather-plots.py
+++ tiny-weather-plots.py
@@ -1,28 +1,30 @@
 #Forecast comparison for 12 hours (now for ICM and AccuWeather)
 
 #Plot for AccuWeather
-
+import sqlite3
+from datetime import datetime
+from itertools import groupby
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-accuweather_data = con.execute("SELECT date_value, value FROM Weather WHERE forecast_period='hour' AND source='AccuWeather' ORDER BY date_value")
-accuweather_data = accuweather_data.fetchall()
+con = sqlite3.connect("db.sqlite")
 
-accuweather_x, accuweather_y = zip(*accuweather_data)
-accuweather_x = [datetime.fromisoformat(x) for x in accuweather_x]
-print(accuweather_x)
-print(accuweather_y)
-
-
-plt.figure(figsize=(10, 6))
-plt.plot(accuweather_x, accuweather_y, label='AccuWeather')
-
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M')) #data na stringa
-plt.legend()
-plt.gcf().autofmt_xdate()
-#plt.xticks(rotation=45)
-plt.grid(True)
-
-plt.savefig('wykres_AccuWeather.png')
-
-#res = con.execute("SELECT value, date_value, source FROM Weather WHERE forecast_period='hour' AND source='AccuWeather' UNION SELECT * FROM (SELECT value, date_value, source FROM Weather WHERE forecast_period='hour' AND source='ICM' ORDER BY date_value LIMIT 12) ORDER BY date_value")
+sources = ["ICM", "AccuWeather"]
+for source in sources:
+    cur = con.execute("""
+            SELECT date_value, value
+            FROM Weather 
+            WHERE forecast_period='hour' AND source=? 
+            ORDER BY source, date_value
+            LIMIT 12
+            """, (source,))
+    
+    data_x, data_y = zip(*cur)
+    for x in data_x:
+        x = datetime.fromisoformat(x)  
+    data_x = [datetime.fromisoformat(x) for x in data_x]
+    print(data_x)
+    print(data_y)   
+    
+    
+   
