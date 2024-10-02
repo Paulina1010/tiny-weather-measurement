9bcd48a927b92ae81cb5b968de601d2c6e2a884d
Paulina1010 <paulina.kucinska10@gmail.com>
2024-10-02 11:09:31 +0200

    Add precipitation probability to 12 h forecast from AccuWeather

--- tiny-weather-load.py
+++ tiny-weather-load.py
@@ -7,11 +7,9 @@ import sys
 
 con = sqlite3.connect("db.sqlite")
 
-
 #ICM
 #Temperature
 #Hourly data for 5 days
-"""
 url = "https://api.meteo.pl/api/v1/model/wrf/grid/d02_XLONG_XLAT/coordinates/332,266/field/T2/level/0/date/2024-09-30T00/forecast/"
 headers = {"Authorization": "Token %s" % sys.argv[1]}
 req = urllib.request.Request(url, headers=headers, method="POST")
@@ -23,11 +21,10 @@ try:
         for t, v in zip(data["times"], data["data"]):
             v = v - 273.15  #kelvin to celcius
             con.execute("INSERT INTO Weather(date_value, metric, value, forecast_period, forecast_date, load_date, source) VALUES(?, ?, ?, ?, ?, ?, ?)", (t, "temperature", v, "hour", forecast_date, load_date, "ICM"))
-            #print(t, v)
         con.commit()
 except urllib.error.HTTPError as e:
     print(e, e.read())
-"""
+
 #Precipitation
 url = "https://api.meteo.pl/api/v1/model/wrf/grid/d02_XLONG_XLAT/coordinates/332%2C266/field/RAINNC/level/0/date/2024-09-30T00/forecast/"
 headers = {"Authorization": "Token %s" % sys.argv[1]}
@@ -38,10 +35,8 @@ with urllib.request.urlopen(req) as f:
     load_date = datetime.now().isoformat()
     for t, v in zip(data["times"], data["data"]):
         con.execute("INSERT INTO Weather(date_value, metric, value, forecast_period, forecast_date, load_date, source) VALUES(?, ?, ?, ?, ?, ?, ?)", (t, "precipitation", v, "hour", forecast_date, load_date, "ICM"))
-        #print(t, v)
     con.commit()
 
-"""
 #AccuWeather - Interia
 #Load forecast for 12h
 url = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/274663?apikey=%s&metric=true" % sys.argv[2] #code for Warsaw
@@ -50,10 +45,14 @@ with urllib.request.urlopen(url) as f:
     forecast_date = datetime.now().isoformat()
     load_date = datetime.now().isoformat()
     for row in data:
+        #print(row)
+        #Temperature
         date_value = row["DateTime"]
         value = row["Temperature"]["Value"]
-        #print(date_value, value)
         con.execute("INSERT INTO Weather(date_value, metric, value, forecast_period, forecast_date, load_date, source) VALUES(?, ?, ?, ?, ?, ?, ?)", (date_value, "temperature", value, "hour", forecast_date, load_date, "AccuWeather"))
+        #PrecipitationProbability
+        value = row["PrecipitationProbability"]
+        con.execute("INSERT INTO Weather(date_value, metric, value, forecast_period, forecast_date, load_date, source) VALUES(?, ?, ?, ?, ?, ?, ?)", (date_value, "precipitation_probability", value, "hour", forecast_date, load_date, "AccuWeather"))
     con.commit()
 
 #Load forecast for 5 days
@@ -64,6 +63,8 @@ with urllib.request.urlopen(url) as f:
     forecast_date = datetime.now().isoformat()
     load_date = datetime.now().isoformat()
     for row in data["DailyForecasts"]:
+        print(row)
+   
         date_value = row['Date']
         value_min = row['Temperature']['Minimum']['Value']
         con.execute("INSERT INTO Weather(date_value, metric, value, forecast_period, forecast_date, load_date, source) VALUES(?, ?, ?, ?, ?, ?, ?)", (date_value, "temperature min", value_min, "daily", forecast_date, load_date, "AccuWeather"))
@@ -72,4 +73,3 @@ with urllib.request.urlopen(url) as f:
         value_max = row['Temperature']['Maximum']['Value']
         con.execute("INSERT INTO Weather(date_value, metric, value, forecast_period, forecast_date, load_date, source) VALUES(?, ?, ?, ?, ?, ?, ?)", (date_value, "temperature max", value_max, "daily", forecast_date, load_date, "AccuWeather"))
     con.commit()
-"""
